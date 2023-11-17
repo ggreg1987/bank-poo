572f6b15de7ed05dc6a1858baed4b532ba3bcd80
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-17 17:46:17 -0300

    Creating a create_account method

--- domain/models/Client.py
+++ domain/models/Client.py
@@ -2,9 +2,17 @@ from abc import ABC
 
 
 class Client(ABC):
-    id: int = 1
+    id: int = 0
 
-    def __int__(self, *args, **kwargs):
+    def __int__(self, account, address):
         self.__id = Client.id
-        self.__accounts: list = args
-        self.__address: dict = kwargs
+        self.__accounts: list = account
+        self.__address: dict = address
+
+    @property
+    def create_account(self, *args, **kwargs):
+        self.__id += 1
+        self.__accounts.append(args)
+        for self.Client, self.__address in kwargs.items():
+            return f"{self}: {self.__address}"
+
