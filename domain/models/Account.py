31b16aa99a1119c60cbd56599e7e826cbd608264
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-18 15:38:39 -0300

    Creating a withdraw method

--- domain/models/Account.py
+++ domain/models/Account.py
@@ -14,3 +14,13 @@ class Account:
 
     def new_account(self, name, date_birth, cpf, agency) -> int:
         return self.__client.new_client((name, date_birth, cpf, agency), {"street", "number"})
+
+    def withdraw(self, value: float):
+        if value > self.WITHDRAW_LIMIT or value < 0 or self.withdraw_limit_per_day == 0:
+            return "Amount incorrect or limit per day"
+        else:
+            self.withdraw_limit_per_day -= 1
+            self.balance -= value
+            result = f"Draw out R${value}"
+            self.__extract.add_extract(result)
+            return result
