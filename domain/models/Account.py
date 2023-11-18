df34787a9c7d277fe3dc7f84a9051755865e2300
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-18 15:05:47 -0300

    Creating an Account class and your constructor method

--- domain/models/Account.py
+++ domain/models/Account.py
@@ -1,2 +1,12 @@
 from Extract import Extract
 from PhysicalPerson import PhysicalPerson
+
+
+class Account:
+    balance: float
+    WITHDRAW_LIMIT = 500
+    withdraw_limit_per_day = 3
+
+    def __int__(self, agency: str, client: PhysicalPerson):
+        self.__agency = agency
+        self.__client = client
