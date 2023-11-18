d0873f9faee933ace59c1b271f811eafd742d138
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-18 14:37:07 -0300

    Creating an Extract class and two methods to add and list extracts

--- domain/models/Extract.py
+++ domain/models/Extract.py
@@ -1,1 +1,11 @@
 
+class Extract:
+    extract: list
+
+    def add_extract(self, value):
+        self.extract.append(value)
+
+    def extract(self):
+        for ex in self.extract:
+            print(ex)
+
