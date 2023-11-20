0d80ab81174c960b600a8d42233c72f6efa9413a
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-20 15:54:10 -0300

    Refactoring PhysicalPerson

--- domain/models/PhysicalPerson.py
+++ domain/models/PhysicalPerson.py
@@ -4,7 +4,9 @@ from Client import Client
 
 
 class PhysicalPerson(Client):
-    def __init__(self, name, date_birth, cpf,  account, address: dict):
+    __id = 0
+
+    def __init__(self, name, date_birth, cpf, account, address: dict):
         super().__init__(account, address)
         self.__name = name
         self.__date_birth = self.str_to_date(date_birth)
@@ -13,7 +15,7 @@ class PhysicalPerson(Client):
     def new_client(self, *args, **kwargs):
         self.__id += 1
         self.__accounts.append(args)
-        for self.Client.__class__.__name__, self.__address in kwargs.items():
+        for args, self.__address in kwargs.items():
             return f"{self}: {self.__address}"
         return self.__id
 
