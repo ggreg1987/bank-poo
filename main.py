9dd337780cae29624cee4bc73eb2332f0f45f29c
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-20 16:56:53 -0300

    Creating first case

--- main.py
+++ main.py
@@ -8,6 +8,27 @@ class Main:
     Wellcome {your_name} to DIO's BANK!!!
     """)
 
+    def start_app(self):
+        print(f"""
+        CHOOSE YOUR OPTION
 
+        1 - CREATE ACCOUNT
+        2 - DEPOSIT
+        3 - WITHDRAW
+        4 - EXTRACT
+        5 - EXIT
+        """)
+        option = 0
 
-
+        while option != 5:
+            option = int(input())
+            match option:
+                case 1:
+                    name = input("Name")
+                    date_birth = input("Birth day")
+                    cpf = input("Cpf")
+                    agency = input("Agency")
+                    street = input("Street")
+                    number = int(input("Number"))
+                    new_account = self.__account.new_account(name, date_birth, cpf, agency, street, number)
+                    print(new_account)
