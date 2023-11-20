9940e2398c3691f38736c87e288fcf094f2ea136
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-20 16:38:58 -0300

    Creating Main class

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from domain.models.Account import Account
 
 
+class Main:
+    __account = Account
+    your_name = input("What's your name?")
+    print(f"""
+    Wellcome {your_name} to DIO's BANK!!!
+    """)
+
+
+
+
